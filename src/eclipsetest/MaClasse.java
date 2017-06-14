d5e59100143003e5398fc204611bf2ed47696ddd
ejhelil <ejhelil@mbldsil20.up7d.priv>
2017-06-14 11:24:57 +0200

    1er push request
    push request pour tester :?

--- src/eclipsetest/MaClasse.java
+++ src/eclipsetest/MaClasse.java
@@ -5,6 +5,8 @@ public class MaClasse {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("MaClasse");
+		// afficher commentaire
+		System.out.println("commentaire");
 	}
 
 }
